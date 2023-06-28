1c16d582d9a131e1d82a5affcdfedd25895b81e0
Juan Guerra <juanguerradasilveira@gmail.com>
2023-06-28 15:03:07 -0300

    fixes bugs and creates db

--- mecajato/clientes/models.py
+++ mecajato/clientes/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Cliente(models.Model):
+    nome = models.CharField(max_length=50)
+    sobrenome = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    sobrenome = models.CharField(max_length=12)
+    
+    def __str__(self) -> str:
+        return self.nome
+    
+class Carro
+    
