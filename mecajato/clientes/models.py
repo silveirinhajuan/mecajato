79923da4eee492a51d93c2d832919cd637d64839
Juan Guerra <juanguerradasilveira@gmail.com>
2023-06-29 00:56:51 -0300

    implemented db in bac, connected back with front

--- mecajato/clientes/models.py
+++ mecajato/clientes/models.py
@@ -4,10 +4,20 @@ class Cliente(models.Model):
     nome = models.CharField(max_length=50)
     sobrenome = models.CharField(max_length=50)
     email = models.EmailField(max_length=50)
-    sobrenome = models.CharField(max_length=12)
+    cpf = models.CharField(max_length=12)
     
     def __str__(self) -> str:
         return self.nome
     
-class Carro
+class Carro(models.Model):
+    carro = models.CharField(max_length=50)
+    placa = models.CharField(max_length=8)
+    ano = models.IntegerField()
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    lavagens = models.BigIntegerField(default=0)
+    consertos = models.BigIntegerField(default=0)
+    
+    def __str__(self) -> str:
+        return self.carro
+    
     
